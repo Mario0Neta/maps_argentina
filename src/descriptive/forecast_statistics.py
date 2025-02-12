a4faeb3a43d92fdc568f753ec014fc94c80659e0
Mario0Neta <mario.neta.rosario@gmail.com>
2025-02-12 23:55:04 +0100

    feat: base model trained and logged with mlflow

--- src/descriptive/forecast_statistics.py
+++ src/descriptive/forecast_statistics.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objects as go
 import numpy as np
+from pathlib import Path
+from dotenv import load_dotenv
 
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.graphics.tsaplots import plot_acf
 
 # %%
-flights_data_path = os.path.join(
-    os.path.expanduser("~"), "Documents", "maps_argentina", "base_microdatos.csv"
-)
+load_dotenv()
+flights_data_path = Path(os.getenv("BASE_DIR")) / "data" / "raw" / "base_microdatos.csv"
+
 vuelos_df = pd.read_csv(flights_data_path)
 vuelos_ar_df = vuelos_df[(vuelos_df["clasificacion_vuelo"] == "Cabotaje")].copy()
 vuelos_cordoba_df = vuelos_ar_df[
@@ -33,52 +35,83 @@ vuelos_cordoba_df = vuelos_cordoba_df.groupby("fecha").agg(
 date_range = pd.date_range(
     start=vuelos_cordoba_df.index.min(), end=vuelos_cordoba_df.index.max(), freq="D"
 )
-vuelos_cordoba_df = vuelos_cordoba_df.sort_values("fecha")
-vuelos_cordoba_df = vuelos_cordoba_df.reindex(date_range).reset_index()
-vuelos_cordoba_df["pasajeros"] = vuelos_cordoba_df["pasajeros"].fillna(0)
 
+vuelos_cordoba_df = vuelos_cordoba_df.sort_values("fecha")
+vuelos_cordoba_df = vuelos_cordoba_df.reindex(date_range)
+vuelos_cordoba_df["pasajeros"] = vuelos_cordoba_df["pasajeros"].fillna(0).astype("int")
+vuelos_cordoba_df = vuelos_cordoba_df.reset_index()
+vuelos_cordoba_df = vuelos_cordoba_df.rename(columns={"index": "fecha"})
 # %%
 ###COVID
 covid_start = pd.to_datetime("01-02-2020")
 covid_end = pd.to_datetime("01-04-2022")
-vuelos_cordoba_df["index"] = pd.to_datetime(vuelos_cordoba_df["index"])
 vuelos_cordoba_df["covid"] = np.where(
-    (vuelos_cordoba_df["index"] >= covid_start)
-    & (vuelos_cordoba_df["index"] < covid_end),
+    (vuelos_cordoba_df["fecha"] >= covid_start)
+    & (vuelos_cordoba_df["fecha"] < covid_end),
     1,
     0,
 )
 # %%
+# Save the file to train.
+min_date = vuelos_cordoba_df["fecha"].min().strftime("%Y%m%d")
+max_date = vuelos_cordoba_df["fecha"].max().strftime("%Y%m%d")
+flights_ready_data_path = Path(os.getenv("BASE_DIR")) / "data" / "cleaned" / "cordoba"/ f"0_{min_date}_{max_date}_full_data.csv"
+
+vuelos_cordoba_df.to_csv(flights_ready_data_path, index=False)
+
+
+#%%
 ####################
 # Train, test and validation
 end_train = pd.to_datetime("01-02-2023")
 end_test = pd.to_datetime("01-02-2024")
-vuelos_cordoba_df = vuelos_cordoba_df.set_index("index")
+
 train_vuelos_cordoba_df = vuelos_cordoba_df[
-    (vuelos_cordoba_df.index <= end_train)
+    (vuelos_cordoba_df["fecha"]  <= end_train)
 ].copy()
 test_vuelos_cordoba_df = vuelos_cordoba_df[
-    (vuelos_cordoba_df.index > end_train) & (vuelos_cordoba_df.index <= end_test)
+    (vuelos_cordoba_df["fecha"]  > end_train) & (vuelos_cordoba_df["fecha"]  <= end_test)
 ].copy()
 validate_vuelos_cordoba_df = vuelos_cordoba_df[
-    vuelos_cordoba_df.index > end_test
+    vuelos_cordoba_df["fecha"]  > end_test
 ].copy()
 print(
-    f"Training will go from {train_vuelos_cordoba_df.index.min()} to {train_vuelos_cordoba_df.index.max()}"
+    f"Training will go from {train_vuelos_cordoba_df['fecha'].min()} to {train_vuelos_cordoba_df['fecha'].max()}"
 )
 print(
-    f"Testing will go from {test_vuelos_cordoba_df.index.min()} to {test_vuelos_cordoba_df.index.max()}"
+    f"Testing will go from {test_vuelos_cordoba_df['fecha'].min()} to {test_vuelos_cordoba_df['fecha'].max()}"
 )
 print(
-    f"Validation will go from {validate_vuelos_cordoba_df.index.min()} to {validate_vuelos_cordoba_df.index.max()}"
+    f"Validation will go from {validate_vuelos_cordoba_df['fecha'].min()} to {validate_vuelos_cordoba_df['fecha'].max()}"
 )
+#%%
+#Save test, train and validation in different files
+min_date = train_vuelos_cordoba_df["fecha"].min().strftime("%Y%m%d")
+max_date = train_vuelos_cordoba_df["fecha"].max().strftime("%Y%m%d")
+train_vuelos_cordoba_df_path = Path(os.getenv("BASE_DIR")) / "data" / "cleaned" / "cordoba" / f"{min_date}_{max_date}_train.csv"
+
+train_vuelos_cordoba_df.to_csv(train_vuelos_cordoba_df_path, index=False)
+
+min_date = test_vuelos_cordoba_df["fecha"].min().strftime("%Y%m%d")
+max_date = test_vuelos_cordoba_df["fecha"].max().strftime("%Y%m%d")
+test_vuelos_cordoba_df_path = Path(os.getenv("BASE_DIR")) / "data" / "cleaned" / "cordoba" / f"{min_date}_{max_date}_test.csv"
+
+test_vuelos_cordoba_df.to_csv(test_vuelos_cordoba_df_path, index=False)
+
+min_date = validate_vuelos_cordoba_df["fecha"].min().strftime("%Y%m%d")
+max_date = validate_vuelos_cordoba_df["fecha"].max().strftime("%Y%m%d")
+validate_vuelos_cordoba_df_path = Path(os.getenv("BASE_DIR")) / "data" / "cleaned" / "cordoba" / f"{min_date}_{max_date}_validation.csv"
+
+validate_vuelos_cordoba_df.to_csv(validate_vuelos_cordoba_df_path, index=False)
+
+
 # %%
 # Data visualization
 
 fig = go.Figure()
 fig.add_trace(
     go.Scatter(
-        x=train_vuelos_cordoba_df.index,
+        x=train_vuelos_cordoba_df["fecha"],
         y=train_vuelos_cordoba_df["pasajeros"],
         mode="lines",
         name="Train",
@@ -86,7 +119,7 @@ fig.add_trace(
 )
 fig.add_trace(
     go.Scatter(
-        x=test_vuelos_cordoba_df.index,
+        x=test_vuelos_cordoba_df["fecha"],
         y=test_vuelos_cordoba_df["pasajeros"],
         mode="lines",
         name="test",
@@ -94,7 +127,7 @@ fig.add_trace(
 )
 fig.add_trace(
     go.Scatter(
-        x=validate_vuelos_cordoba_df.index,
+        x=validate_vuelos_cordoba_df["fecha"],
         y=validate_vuelos_cordoba_df["pasajeros"],
         mode="lines",
         name="validation",
@@ -115,7 +148,7 @@ fig.update_layout(
 fig, axs = plt.subplots(2, 2, figsize=(8, 5), sharex=False, sharey=True)
 axs = axs.ravel()
 
-vuelos_cordoba_df["mes"] = vuelos_cordoba_df.index.month
+vuelos_cordoba_df["mes"] = vuelos_cordoba_df["fecha"].month
 vuelos_cordoba_df[vuelos_cordoba_df["covid"] == 0].boxplot(
     column="pasajeros", by="mes", ax=axs[0], flierprops={"markersize": 3, "alpha": 0.3}
 )
@@ -134,7 +167,7 @@ vuelos_cordoba_df.groupby("mes")["pasajeros"].median().plot(
 axs[1].set_ylabel("pasajeros")
 axs[1].set_title("Pasajeros por mes", fontsize=9)
 
-vuelos_cordoba_df["semana"] = vuelos_cordoba_df.index.day_of_week + 1
+vuelos_cordoba_df["semana"] = vuelos_cordoba_df["fecha"].day_of_week + 1
 vuelos_cordoba_df[vuelos_cordoba_df["covid"] == 0].boxplot(
     column="pasajeros",
     by="semana",
